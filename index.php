9c9398d74ad9b7d1d415fca7403955fc82a067bf
Anne <lutfiane.fadila@gmail.com>
2021-07-04 09:20:34 +0700

    ubah title

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once 'connect_db.php';
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Latihan CRUD</title>
+  <title>CRUD dengan PHP</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   <link rel="stylesheet" href="style.css">
 </head>
--- update.php
+++ update.php
@@ -12,7 +12,7 @@ if (isset($_GET['id'])) {
   <!DOCTYPE html>
     <html>
     <head>
-      <title>Latihan CRUD - Ubah</title>
+      <title>CRUD dengan PHP - Ubah</title>
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
       <link rel="stylesheet" href="style.css">
     </head>
